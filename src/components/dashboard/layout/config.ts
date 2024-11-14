631f8ac92c677060c7da4308d861f0fe04d09bac
prashantsayp <prashantkr0111@gmail.com>
2024-11-14 10:35:06 +0530

    sidebar-tabs-fix-done

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -4,8 +4,11 @@ import { paths } from '@/paths';
 export const navItems = [
   { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
   { key: 'appointment', title: 'Appointment', href: paths.dashboard.customers, icon: 'users' },
+  { key: 'doctor', title: 'Doctor', href: paths.dashboard.doctor, icon: 'user' },
+  { key: 'patient', title: 'Patient', href: paths.dashboard.patient, icon: 'users' },
+  { key: 'speciality', title: 'Speciality', href: paths.dashboard.speciality, icon: 'local-pharmacy' },
+  { key: 'symptoms', title: 'Symptoms', href: paths.dashboard.symptoms, icon: 'medical-services' },
   { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
   { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
-  { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
   { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
 ] satisfies NavItemConfig[];
