392765018c17cd029916960840ec8fc5f47b324b
coder-faraz <hfaraz316@gmail.com>
2025-04-02 12:47:52 +0530

    eslint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     // require.resolve('@vercel/style-guide/eslint/browser'),
     require.resolve('@vercel/style-guide/eslint/react'),
     require.resolve('@vercel/style-guide/eslint/next'),
+    "next/core-web-vitals"
   ],
   parserOptions: {
     project,
@@ -43,9 +44,9 @@ module.exports = {
         ignoreOnInitialization: true,
       },
     ],
-    'import/newline-after-import': 'error',
-    'react/jsx-uses-react': 'error',
-    'react/react-in-jsx-scope': 'error',
+    // 'import/newline-after-import': 'error',
+    // 'react/jsx-uses-react': 'error',
+    // 'react/react-in-jsx-scope': 'error',
     'unicorn/filename-case': [
       'error',
       {
@@ -57,26 +58,28 @@ module.exports = {
     ],
 
     // Deactivated
-    '@typescript-eslint/dot-notation': 'off', // paths are used with a dot notation
-    '@typescript-eslint/no-misused-promises': 'off', // onClick with async fails
-    '@typescript-eslint/no-non-null-assertion': 'off', // sometimes compiler is unable to detect
-    '@typescript-eslint/no-unnecessary-condition': 'off', // remove when no static data is used
-    '@typescript-eslint/require-await': 'off', // Server Actions require async flag always
-    '@typescript-eslint/prefer-nullish-coalescing': 'off', // personal style
-    '@typescript-eslint/restrict-template-expressions': [
-      'error',
-      {
-        allowNumber: true,
-      },
-    ],
-    'import/no-default-export': 'off', // Next.js components must be exported as default
-    'import/no-extraneous-dependencies': 'off', // conflict with sort-imports plugin
-    'import/order': 'off', // using custom sort plugin
-    'no-nested-ternary': 'off', // personal style
-    'no-redeclare': 'off', // conflict with TypeScript function overloads
-    'react/jsx-fragments': 'off', // personal style
-    'react/prop-types': 'off', // TypeScript is used for type checking
+    // 'no-const-assign': 'off',
+    // 'no-shadow': 'off',
+    // '@typescript-eslint/dot-notation': 'off', // paths are used with a dot notation
+    // '@typescript-eslint/no-misused-promises': 'off', // onClick with async fails
+    // '@typescript-eslint/no-non-null-assertion': 'off', // sometimes compiler is unable to detect
+    // '@typescript-eslint/no-unnecessary-condition': 'off', // remove when no static data is used
+    // '@typescript-eslint/require-await': 'off', // Server Actions require async flag always
+    // '@typescript-eslint/prefer-nullish-coalescing': 'off', // personal style
+    // '@typescript-eslint/restrict-template-expressions': [
+    //   'error',
+    //   {
+    //     allowNumber: true,
+    //   },
+    // ],
+    // 'import/no-default-export': 'off', // Next.js components must be exported as default
+    // 'import/no-extraneous-dependencies': 'off', // conflict with sort-imports plugin
+    // 'import/order': 'off', // using custom sort plugin
+    // 'no-nested-ternary': 'off', // personal style
+    // 'no-redeclare': 'off', // conflict with TypeScript function overloads
+    // 'react/jsx-fragments': 'off', // personal style
+    // 'react/prop-types': 'off', // TypeScript is used for type checking
 
-    '@next/next/no-img-element': 'off', // Temporary disabled
+    // '@next/next/no-img-element': 'off', // Temporary disabled
   },
 };
