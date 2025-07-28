eccaa219afa75efa3fdb1dfe9d801653d3354379
Adarsh Kumar <kumaradarsh79301@gmail.com>
2025-07-28 18:44:30 +0530

    fixed build error

--- src/app/(auth)/signup-as-doctor/page.tsx
+++ src/app/(auth)/signup-as-doctor/page.tsx
@@ -1,8 +1,9 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { Layout } from '@/components/auth/layout';
-import { DoctorSignup } from '@/components/auth/doctor-signup-form';
+export const dynamic = "force-dynamic";
+import { useState } from "react";
+import { Layout } from "@/components/auth/layout";
+import { DoctorSignup } from "@/components/auth/doctor-signup-form";
 
 export default function Page(): React.JSX.Element {
   const [clientRole, setClientRole] = useState<string | null>(null);
